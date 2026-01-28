5aaa986dddf42091a76b13f2b5c4b4a72ac5df29
MoniVibe <shonhay@gmail.com>
2026-01-28 20:45:44 +0200

    Headless: log nonzero exit requests with stack

--- Packages/com.moni.puredots/Runtime/Runtime/Time/HeadlessExitUtility.cs
+++ Packages/com.moni.puredots/Runtime/Runtime/Time/HeadlessExitUtility.cs
@@ -1,4 +1,6 @@
+using System;
 using Unity.Entities;
+using UnityEngine;
 using PureDOTS.Runtime.Components;
 
 namespace PureDOTS.Runtime.Time
@@ -7,6 +9,13 @@ namespace PureDOTS.Runtime.Time
     {
         public static void Request(EntityManager entityManager, uint tick, int exitCode)
         {
+            if (exitCode != 0)
+            {
+                Debug.LogError(
+                    $"[HeadlessExitUtility] Request exit_code={exitCode} tick={tick}\n" +
+                    Environment.StackTrace);
+            }
+
             using var query = entityManager.CreateEntityQuery(ComponentType.ReadWrite<HeadlessExitRequest>());
             var entity = query.IsEmptyIgnoreFilter
                 ? entityManager.CreateEntity(typeof(HeadlessExitRequest))
@@ -20,4 +29,3 @@ namespace PureDOTS.Runtime.Time
         }
     }
 }
-
