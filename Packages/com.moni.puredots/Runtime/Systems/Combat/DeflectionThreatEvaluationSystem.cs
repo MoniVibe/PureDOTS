714055c8285a8dfb48db9c75a7a42357edc25734
MoniVibe <shonhay@gmail.com>
2026-02-11 18:22:42 +0200

    puredots: add Unity.Collections imports for combat FixedString types

--- Packages/com.moni.puredots/Runtime/Systems/Combat/DeflectionThreatEvaluationSystem.cs
+++ Packages/com.moni.puredots/Runtime/Systems/Combat/DeflectionThreatEvaluationSystem.cs
@@ -2,6 +2,7 @@ using PureDOTS.Runtime.Combat;
 using PureDOTS.Runtime.Components;
 using PureDOTS.Runtime.LowLevel;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
